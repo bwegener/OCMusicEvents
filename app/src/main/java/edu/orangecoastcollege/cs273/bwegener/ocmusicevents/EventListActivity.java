20e8760a47b881de36fcebdecca21160a6a08b53
bwegener <wegener_brian@yahoo.com>
2017-09-21 14:52:53 -0700

    Update 2:52pm 9.21.17

--- app/src/main/java/edu/orangecoastcollege/cs273/bwegener/ocmusicevents/EventListActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/bwegener/ocmusicevents/EventListActivity.java
@@ -1,13 +1,18 @@
 package edu.orangecoastcollege.cs273.bwegener.ocmusicevents;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.ListActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 
-public class EventListActivity extends AppCompatActivity {
+public class EventListActivity extends ListActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_list);
+
+        // Define a built-in list adapter for this ListActivity:
+        setListAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, MusicEvent.titles));
     }
 }
